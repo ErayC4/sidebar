b1d107dc050bd41d32aa444c4e40bf86e5664d7b
ErayC4 <eray04.coskun@gmail.com>
2024-02-18 01:13:18 +0100

    added tailwind and sidebar button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import "./index.css"
 
 function App() {
+  const [sidebarButton, setSidebarButton] = React.useState(false)
 
+  function clickHandler() {
+    setSidebarButton(prevSidebarButton => !prevSidebarButton)
+  }
+
+  const sidebarContent = (
+    <h1>SideBar</h1>
+  )
   return (
     <>
-
+    <button onClick={clickHandler}>{sidebarButton?"hide" : "display"}</button>
+    {sidebarButton && sidebarContent}
+    <p>hi</p>
     </>
   )
 }

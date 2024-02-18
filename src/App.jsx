2296b0b0d44b5acc9bb228ddd00e1b65a042fc4f
ErayC4 <eray04.coskun@gmail.com>
2024-02-18 20:03:50 +0100

    working sidebar, when button is clicked, it shows the sidebarContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import "./index.css"
-
+import React from "react";
+import "./index.css";
 function App() {
-  const [sidebarButton, setSidebarButton] = React.useState(false)
+  const [sidebarButtonOpen, setSidebarButtonOpen] = React.useState(false);
 
   function clickHandler() {
-    setSidebarButton(prevSidebarButton => !prevSidebarButton)
+    setSidebarButtonOpen((prevSidebarButton) => !prevSidebarButton);
   }
 
   const sidebarContent = (
-    <h1>SideBar</h1>
-  )
+    <div className="flex items-center justify-center bg-green-500 h-screen w-32">
+      <p className="text-white text-2xl">Lorem ipsum dolor sit amet</p>
+    </div>
+  );
+
   return (
-    <>
-    <button onClick={clickHandler}>{sidebarButton?"hide" : "display"}</button>
-    {sidebarButton && sidebarContent}
-    <p>hi</p>
-    </>
-  )
+    <div>
+      <div className="fixed inset-y-0 flex">
+        {sidebarButtonOpen && sidebarContent}
+        <button onClick={clickHandler} className="ml-2">
+          {sidebarButtonOpen ? (
+            <img src="../public/chevron-compact-left.svg" alt="" />
+          ) : (
+            <img src="../public/chevron-compact-right.svg" alt="" />
+          )}
+        </button>
+      </div>
+
+      <p className="ml-8 bg-blue-400 text-2xl w-full">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quas eos
+        corrupti rem ullam incidunt nulla culpa quod, minus non dolor molestiae
+        voluptas error? Odit aliquam nulla natus inventore nostrum.
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
